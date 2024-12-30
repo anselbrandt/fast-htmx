b8324bdcb600237cf1f5597c64e33e38a10e7132
anselbrandt <ansel@anselbrandt.com>
2024-12-29 20:01:42 -0800

    Refactor mock data

---
+++ data.py
@@ -0,0 +1,22 @@
+users = [
+    {"id": 0, "name": "Joe Smith", "email": "joe@smith.org", "status": "Active"},
+    {
+        "id": 1,
+        "name": "Angie MacDowell",
+        "email": "angie@macdowell.org",
+        "status": "Active",
+    },
+    {
+        "id": 2,
+        "name": "Fuqua Tarkenton",
+        "email": "fuqua@tarkenton.org",
+        "status": "Active",
+    },
+    {"id": 3, "name": "Kim Yee", "email": "kim@yee.org", "status": "Inactive"},
+]
+
+tasks = [
+    {"id": 123, "name": "one-two-three"},
+    {"id": 456, "name": "four-five-six"},
+    {"id": 789, "name": "seven-eight-nine"},
+]
